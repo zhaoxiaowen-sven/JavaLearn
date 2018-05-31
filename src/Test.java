4e77981e1589730ab341817694eabe3a82d1a6d1
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2018-05-31 16:00:13 +0800

    prototype end

---
+++ src/Test.java
@@ -0,0 +1,82 @@
+import builder.base1.Builder;
+import builder.base1.Director;
+import builder.base1.Product;
+import builder.base2.ActorBuilder;
+import builder.base2.ActorController;
+import builder.base2.AngelBuilder;
+import factory.Factory;
+import factory.VivoFactory;
+import prototype.Attachment;
+import prototype.Weeklylog;
+import prototype.Weeklylog2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args){
+//        Factory vivoFactory = new VivoFactory();
+//        vivoFactory.createMobile().call();
+//        vivoFactory.createTv().play();
+//
+//
+//        Director director = new Director(new Builder());
+//        Product product = director.construct();
+//        System.out.println(product.toString());
+
+//        ActorController actorController = new ActorController();
+//        System.out.println(actorController.construct(new AngelBuilder()));
+
+        //建造者模式
+//        ActorBuilder actorBuilder = new AngelBuilder();
+//        actorBuilder.buildName("name");
+//        actorBuilder.buildAge("age");
+//        actorBuilder.buildFace("name");
+//        System.out.println(actorBuilder.build());
+
+        //----------------------原型模式 begin---------------
+        Weeklylog2 log_previous = new Weeklylog2();  //创建原型对象
+        log_previous.name = "vivo";
+        log_previous.date = "0530";
+        log_previous.tasks = new ArrayList();
+        Attachment attachment = new Attachment();
+        attachment.setName("attachment");
+        log_previous.attachment = attachment;
+
+        Weeklylog2 today = null;
+        try {
+            today = log_previous.deepClone();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+//        System.out.println(log_previous == today);
+//        System.out.println(log_previous.name == today.name);
+//        System.out.println(log_previous.date == today.date);
+//        System.out.println(log_previous.tasks == today.tasks);
+//        System.out.println(log_previous.attachment == today.attachment);
+//
+//        System.out.println(log_previous.name + " "+ today.name);
+//        System.out.println(log_previous.date + " "+ today.date);
+        //----------------------prototype end---------------
+
+        Weeklylog log_previous2 = new Weeklylog();  //创建原型对象
+        log_previous2.name = "vivo";
+        log_previous2.date = "0530";
+        log_previous2.tasks = new ArrayList();
+        Attachment attachment2 = new Attachment();
+        attachment2.setName("yyy");
+        attachment.setName("attachment");
+        log_previous2.attachment = attachment2;
+
+        Weeklylog today2 = log_previous2.clone();
+        log_previous2.name = "xxx";
+        today2.attachment.setName("yyy2");
+        System.out.println(log_previous2 == today2);
+        System.out.println(log_previous2.name +"//"+ today2.name);
+        System.out.println(log_previous2.attachment.getName() +"___"+ today2.attachment.getName());
+
+
+    }
+}
--- src/factory/Test.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-public class Test {
-    public static void main(String[] args){
-        Factory vivoFactory = new VivoFactory();
-        vivoFactory.createMobile().call();
-        vivoFactory.createTv().play();
-    }
-}
