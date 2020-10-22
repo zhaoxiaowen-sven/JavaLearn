7a4ad54fecaae754bbf11de3aa8ac7d54162e897
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2020-10-22 14:38:02 +0800

    solution 008
    Change-Id: Ice3cca27506e8ee511be50885c07441e89e3be57

--- leetcode/src/top100/Solution008.java
+++ leetcode/src/top100/Solution008.java
@@ -16,13 +16,56 @@ public class Solution008 {
         return sb.toString();
     }
 
+    public String intToRoman2(int num) {
+        String r = "";
+        int count = 0;
+        while (num != 0) {
+            int pop = num % 10;
+            // 从个位数向前不断迭代加
+            r = getRoman(pop, count) + r;
+            count++;
+            num /= 10;
+        }
+        return r;
+    }
+
+    public String getRoman(int num, int count) { //count 表示当前的位数，个位，十位...
+        char[] ten = {'I', 'X', 'C', 'M'}; //1,10,100,1000
+        char[] five = {'V', 'L', 'D'};//5,50,500
+        String r = "";
+        if (num <= 3) {
+            while (num != 0) {
+                r += ten[count];
+                num--;
+            }
+        }
+        if (num == 4) {
+            r = (ten[count] + "") + (five[count] + "");
+        }
+        if (num == 5) {
+            r = five[count] + "";
+        }
+        if (num > 5 && num < 9) {
+            r = five[count] + "";
+            num -= 5;
+            while (num != 0) {
+                r += ten[count];
+                num--;
+            }
+        }
+        if (num == 9) {
+            r = (ten[count] + "") + (ten[count + 1] + "");
+        }
+        return r;
+    }
+
     public void test() {
 //        System.out.println(intToRoman(3));
 //        System.out.println(intToRoman(4));
 //        System.out.println(intToRoman(9));
         System.out.println(intToRoman(79));
-        System.out.println(intToRoman(50));
-        System.out.println(intToRoman(1994));
-        System.out.println(intToRoman(2994));
+//        System.out.println(intToRoman(50));
+//        System.out.println(intToRoman(1800));
+//        System.out.println(intToRoman(2994));
     }
 }
