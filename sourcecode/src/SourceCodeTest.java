ede26801848ac4c744b7790cdbaf06eda5cd3bb7
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2021-03-10 18:10:17 +0800

    source
    Change-Id: I2e25d940a36272f188a20b395347b17d27ed0e6f

--- sourcecode/src/SourceCodeTest.java
+++ sourcecode/src/SourceCodeTest.java
@@ -1,7 +1,16 @@
+import java.awt.*;
 import java.util.HashMap;
 
 public class SourceCodeTest {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+
+        int oldCap = 10;
+        int newCap;
+
+        if ((newCap = oldCap << 1) < 10) {
+
+        }
+        System.out.println("newCap " + newCap);
     }
 }
