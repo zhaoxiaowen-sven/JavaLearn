f03a5596f295021ec5650412b98867f6e110ba86
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2021-04-09 17:31:26 +0800

    init jvm
    Change-Id: I09ce57b96fae28f49f04da5278d66a709c8669f1

---
+++ collections/src/CollectionTest.java
@@ -0,0 +1,6 @@
+public class CollectionTest {
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ jvm/src/JvmTest.java
@@ -0,0 +1,5 @@
+public class JvmTest {
+    public static void main(String[] args){
+
+    }
+}
