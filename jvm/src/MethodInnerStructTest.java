a507b2ccc0158a6db3f75105860950fa1b5fe4da
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2021-04-16 17:43:40 +0800

    method area
    Change-Id: I7040683a0b9a2328685d2c36dbc207562aed67d7

---
+++ jvm/src/MethodInnerStructTest.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+class MethodInnerStrucTest extends Object implements Comparable<String>, Serializable {
+    //属性
+    public int num = 10;
+    private long xx = 1001L;
+    public long yy = 1000L;
+    private String nihao = "hello world";
+    private static String str = "测试方法的内部结构";
+    //构造器没写
+
+    //方法
+    public void test1() {
+        int count = 20;
+        System.out.println("count = " + count);
+    }
+
+    public static int test2(int cal) {
+        int result = 0;
+        try {
+            int value = 30;
+            result = value / cal;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public int compareTo(String o) {
+        return 0;
+    }
+}
