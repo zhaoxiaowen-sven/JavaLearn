781079c059d78f0a0afb4e7eaf2184444f78e773
zhaoxiaowen <zhaoxiaowen@bbktel.com>
2021-04-20 22:31:14 +0800

    collection
    Change-Id: I65f0ed495055024949d0225eeafd105559e74563

--- collections/src/CollectionTest.java
+++ collections/src/CollectionTest.java
@@ -1,15 +1,64 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class CollectionTest {
     public static void main(String[] args) {
+//
+//        Collections.synchronizedList(new ArrayList<>());
+//
+//
+//        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+//        concurrentHashMap.put("x", "y");
 
-        Collections.synchronizedList(new ArrayList<>());
+        System.out.println("CollectionTest");
+//         testIterator();
+        testNormal();
+        testForEach();
+
+        LinkedList<String> list = new LinkedList<>();
+        HashSet<String> set = new HashSet<>();
 
 
-        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
-        concurrentHashMap.put("x", "y");
+    }
 
+    private static void testNormal() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        for (int i = 0; i < arrayList.size(); i++) {
+            arrayList.remove("b");
+            System.out.println(arrayList.get(i));
+
+        }
+    }
+
+    private static void testForEach() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        for (String s : arrayList) {
+            System.out.println(s);
+            arrayList.remove("b");
+        }
+    }
+
+    private static void testIterator() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        Iterator<String> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            String str = iterator.next();
+            if ("b".equals(str)) {
+                iterator.remove();
+                arrayList.remove("b");
+            }
+        }
     }
 }
