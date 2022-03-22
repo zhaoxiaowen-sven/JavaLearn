953b3426a7a18cc500c7c6e3e02f7b22af569599
zhaoxiaowen-sven <zhaoxiaowen-sven@gmail.com>
2022-03-22 23:15:54 +0800

    juc0321

--- leetcode/src/category/tree/ancestor/Solution236.java
+++ leetcode/src/category/tree/ancestor/Solution236.java
@@ -1,6 +1,7 @@
 package category.tree.ancestor;
 
 import category.tree.TreeNode;
+import category.tree.dfs.base.Solution094;
 
 public class Solution236 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -24,5 +25,18 @@ public class Solution236 {
         } else {
             return null;
         }
+    }
+
+    public static void main(String[] args) {
+        //  5
+        // 4  6
+        //1 2
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode4 = new TreeNode(4, treeNode1, treeNode2);
+        TreeNode treeNode5 = new TreeNode(5, treeNode4, treeNode6);
+        TreeNode node = new Solution236().lowestCommonAncestor(treeNode5, treeNode1, treeNode4);
+        System.out.println(node.val);
     }
 }
